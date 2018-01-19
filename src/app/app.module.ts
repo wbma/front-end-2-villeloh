f9e5f965d18a8b9955f03ff06fb83d7321ddd88d
Ville Alla <villelohkovuori1@gmail.com>
2018-01-19 12:54:30 +0200

    mostly done (formatting...)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HttpTestComponent } from './http-test/http-test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HttpTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/http-test/http-test.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-http-test',
+  templateUrl: './http-test.component.html',
+  styleUrls: ['./http-test.component.scss']
+})
+export class HttpTestComponent implements OnInit {
+
+  localData: string;
+  remoteData: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+
+    this.loadLocalJson();
+    this.loadRemoteJson();
+
+  }
+
+
+  loadLocalJson() {
+
+    this.http.get('/assets/tsconfig.app.json')
+    .subscribe((res: Response) => this.localData = JSON.stringify(res));
+  }
+
+  loadRemoteJson() {
+
+    this.http.get('http://media.mw.metropolia.fi/wbma/media')
+    .subscribe((res: Response) => this.remoteData = JSON.stringify(res));
+  }
+
+}
